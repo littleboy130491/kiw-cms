ed0fc0e89fd042b0b5d2c7796ad36cd513c2aa74
unknown <littleboy_13@yahoo.com>
2025-06-18 17:52:48 +0700

    Use glob to include all js and css files in vite

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
+import { glob } from 'glob';
+
+const jsFiles = glob.sync('resources/js/*.js');
+const cssFiles = glob.sync('resources/css/*.css');
+
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                ...cssFiles,
+                ...jsFiles,
+            ],
             refresh: true,
         }),
         tailwindcss(),
