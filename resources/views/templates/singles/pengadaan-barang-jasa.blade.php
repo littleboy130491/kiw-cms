b1024f7d98531102cc996d67b1fc07f898f0b9c5
unknown <littleboy_13@yahoo.com>
2025-07-15 17:39:12 +0700

    Update button

--- resources/views/templates/singles/pengadaan-barang-jasa.blade.php
+++ resources/views/templates/singles/pengadaan-barang-jasa.blade.php
@@ -127,10 +127,15 @@
                 <!--Content-->
                 <x-loop.tender-grid qty="4" />
                 <!--button mobile only-->
-                <a class="w-fit btn1 mobile-only back mt-7 lg:hidden" data-aos="fade-down"
-                    href="{{ $tender_archive_url }}">Kembali
+                <a class="w-fit btn1 mobile-only mt-7 lg:hidden" data-aos="fade-down"
+                    href="{{ $tender_archive_url }}">Lihat Semua
                     <span>
-                        <x-icon.arrow-back-white />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                            <path d="M5 12H19" stroke="white" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                            <path d="M12 5L19 12L12 19" stroke="white" stroke-width="2" stroke-linecap="round"
+                                stroke-linejoin="round" />
+                        </svg>
                     </span>
                 </a>
 
