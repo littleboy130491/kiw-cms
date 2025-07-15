c40aa448e7e68d00b8c7ee84bf224d90310c2c75
unknown <littleboy_13@yahoo.com>
2025-07-15 17:37:13 +0700

    Fix css sub-submenu

--- resources/views/components/menu/sub-parent-menu.blade.php
+++ resources/views/components/menu/sub-parent-menu.blade.php
@@ -1,6 +1,9 @@
-<a href="{{ $url ?? '' }}" class="flex items-center justify-between px-4 py-2 text-[var(--color-heading)] hover:bg-[var(--color-lightblue)] hover:text-white">
+<a href="{{ $url ?? '' }}"
+    class="flex text-[.9em] items-center justify-between px-4 py-2 text-[var(--color-heading)] hover:bg-[var(--color-lightblue)] hover:text-white">
     {{ $menu ?? '' }}
     <svg class="ml-2 h-3 w-3" fill="currentColor" viewBox="0 0 20 20">
-    <path fill-rule="evenodd" d="M7.21 5.23a.75.75 0 011.06 0l4.65 4.25a.75.75 0 010 1.08l-4.65 4.25a.75.75 0 11-1.06-1.06L10.293 10 7.21 6.97a.75.75 0 010-1.06z" clip-rule="evenodd" />
+        <path fill-rule="evenodd"
+            d="M7.21 5.23a.75.75 0 011.06 0l4.65 4.25a.75.75 0 010 1.08l-4.65 4.25a.75.75 0 11-1.06-1.06L10.293 10 7.21 6.97a.75.75 0 010-1.06z"
+            clip-rule="evenodd" />
     </svg>
 </a>
