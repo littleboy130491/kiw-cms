850d8ebda27005c7fbcf2c70cd3fdaa62c7f86a9
unknown <littleboy_13@yahoo.com>
2025-07-15 14:07:35 +0700

    Update service container to inject GeneralSettings

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Littleboy130491\Sumimasen\Settings\GeneralSettings;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton('settings', function ($app) {
+            return $app->make(GeneralSettings::class);
+        });
     }
 
     /**
