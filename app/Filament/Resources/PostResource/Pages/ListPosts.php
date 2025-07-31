fe5c91787a7b0870f2b7f14bb6a20767e7d91498
unknown <littleboy_13@yahoo.com>
2025-07-31 15:17:45 +0700

    Update Post Exporter header action from package

--- app/Filament/Resources/PostResource/Pages/ListPosts.php
+++ app/Filament/Resources/PostResource/Pages/ListPosts.php
@@ -2,8 +2,7 @@
 
 namespace App\Filament\Resources\PostResource\Pages;
 
-use App\Filament\Exports\PostExporter;
-use App\Filament\Imports\PostImporter;
+use Littleboy130491\Sumimasen\Filament\Exports\PostExporter;
 use App\Filament\Resources\PostResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
@@ -15,8 +14,6 @@ class ListPosts extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\ImportAction::make()
-                ->importer(PostImporter::class),
             Actions\ExportAction::make()
                 ->exporter(PostExporter::class),
             Actions\CreateAction::make(),
