55f58aa1c632d750a2f26513fa81e5d373f08bdb
unknown <littleboy_13@yahoo.com>
2025-10-10 10:55:20 +0700

    Add conditional block

--- resources/views/templates/singles/fasilitas.blade.php
+++ resources/views/templates/singles/fasilitas.blade.php
@@ -20,26 +20,30 @@
 
         <!--Start Fasilitas Content-->
         <section id="fasilitas">
-            <div class="flex flex-col gap-6 lg:max-w-[1200px] lg:mx-auto lg:gap-6 my-18 lg:my-30 lg:px-0 px-4 sm:px-6">
-                <h2>{{ $fasilitasTitle['fasilitasUtama'] }}</h2>
-                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-                <!--item-->
-                @foreach ($fasilitasUtama as $item)
-                    <x-loop.fasilitas-loop :item="$item" />
-                @endforeach
-                </div>
-            </div>
-
-            <div class="lg:py-30 py-18 bg-[var(--color-transit)]">
-                <div class="flex flex-col overflow-hidden relative lg:gap-9 sm:gap-7 gap-7 lg:px-0 lg:lg:max-w-[1200px] lg:mx-auto sm:px-6 px-4">
-                    <h2>{{ $fasilitasTitle['fasilitasPenunjang'] }}</h2>
+            @if($fasilitasUtama->isNotEmpty())
+                <div class="flex flex-col gap-6 lg:max-w-[1200px] lg:mx-auto lg:gap-6 my-18 lg:my-30 lg:px-0 px-4 sm:px-6">
+                    <h2>{{ $fasilitasTitle['fasilitasUtama'] }}</h2>
                     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
-                    @foreach ($fasilitasPenunjang as $item)
+                    <!--item-->
+                    @foreach ($fasilitasUtama as $item)
                         <x-loop.fasilitas-loop :item="$item" />
                     @endforeach
                     </div>
                 </div>
-            </div>
+            @endif
+
+            @if($fasilitasPenunjang->isNotEmpty())
+                <div class="lg:py-30 py-18 bg-[var(--color-transit)]">
+                    <div class="flex flex-col overflow-hidden relative lg:gap-9 sm:gap-7 gap-7 lg:px-0 lg:lg:max-w-[1200px] lg:mx-auto sm:px-6 px-4">
+                        <h2>{{ $fasilitasTitle['fasilitasPenunjang'] }}</h2>
+                        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
+                        @foreach ($fasilitasPenunjang as $item)
+                            <x-loop.fasilitas-loop :item="$item" />
+                        @endforeach
+                        </div>
+                    </div>
+                </div>
+            @endif
         </section>
 
         <!--Popup Content-->
