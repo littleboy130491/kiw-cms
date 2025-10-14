67a0bcf75aee16a18f7cefc1f1d6554bec33efc2
unknown <littleboy_13@yahoo.com>
2025-10-14 13:14:17 +0700

    Ekosistem klaster bisnis DONE

--- resources/views/templates/singles/ekosistem-klaster-bisnis.blade.php
+++ resources/views/templates/singles/ekosistem-klaster-bisnis.blade.php
@@ -1,74 +1,29 @@
 @php
-//Start Tab Sektor Industri Temporary Data
-    $tabSektorIndustri = [
-        'tabsTitle' => [
-            ['tab' => 'Modern Textile & Garment'],
-            ['tab' => 'Wood & Furniture'],
-            ['tab' => 'Chemical & New Material'],
-            ['tab' => 'Consumer Goods & Food Procesing'],
-            ['tab' => 'Others'],
-        ],
+    $tabSektorIndustri = [];
+    
+    if (isset($item->block) && is_array($item->block)) {
+        foreach ($item->block as $block) {
+            if ($block['data']['block_id'] === 'tab') {
+                $data = $block['data'];
+                $tabSektorIndustri[] = [
+                    'tabTitle' => $data['title'] ?? '',
+                    'tabContent' => [
+                        'title' => $data['subtitle'] ?? '',
+                        'description' => $data['description'] ?? '',
+                        'image' => $data['media_url'] ?? '',
+                    ]
+                ];
+            }
+        }
+    }
 
-        'tabsContent' => [
-            [ 'tab' => [
-                'tabContentLeft' => [
-                    'title'=>'Modern Textile and Garment Industry',
-                    'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a augue in erat fermentum imperdiet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed sodales semper tincidunt. Curabitur varius ultricies magna eleifend tincidunt. Suspendisse fringilla malesuada metus eu rutrum. Proin neque ante, fermentum sed hendrerit eget, scelerisque at risus. In posuere dui a neque dictum placerat.',
-                ],
-                'tabContentRight' => [
-                    'image'=> Storage::url('media/garmen.png'),
-                ],
-            ]], 
-
-            [ 'tab' => [
-                'tabContentLeft' => [
-                    'title'=>'Wood & Furniture Industry',
-                    'desc'=>'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a augue in erat fermentum imperdiet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed sodales semper tincidunt. Curabitur varius ultricies magna eleifend tincidunt. Suspendisse fringilla malesuada metus eu rutrum. Proin neque ante, fermentum sed hendrerit eget, scelerisque at risus. In posuere dui a neque dictum placerat.',
-                ],
-                'tabContentRight' => [
-                    'image'=> Storage::url('media/furniture.png'),
-                ],
-            ]],
-
-            [ 'tab' => [
-                'tabContentLeft' => [
-                    'title'=>'Chemical & New Material Industry',
-                    'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a augue in erat fermentum imperdiet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed sodales semper tincidunt. Curabitur varius ultricies magna eleifend tincidunt. Suspendisse fringilla malesuada metus eu rutrum. Proin neque ante, fermentum sed hendrerit eget, scelerisque at risus. In posuere dui a neque dictum placerat.',
-                ],
-                'tabContentRight' => [
-                    'image'=> Storage::url('media/chemical.png'),
-                ],
-            ]],
-
-            [ 'tab' => [
-                'tabContentLeft' => [
-                    'title'=>'Consumer Goods & Food Procesing Industry',
-                    'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a augue in erat fermentum imperdiet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed sodales semper tincidunt. Curabitur varius ultricies magna eleifend tincidunt. Suspendisse fringilla malesuada metus eu rutrum. Proin neque ante, fermentum sed hendrerit eget, scelerisque at risus. In posuere dui a neque dictum placerat.',
-                ],
-                'tabContentRight' => [
-                    'image'=> Storage::url('media/consumer.png'),
-                ],
-            ]],
-
-            [ 'tab' => [
-                'tabContentLeft' => [
-                    'title'=>'Others',
-                    'desc' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut a augue in erat fermentum imperdiet. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed sodales semper tincidunt. Curabitur varius ultricies magna eleifend tincidunt. Suspendisse fringilla malesuada metus eu rutrum. Proin neque ante, fermentum sed hendrerit eget, scelerisque at risus. In posuere dui a neque dictum placerat.',
-                ],
-                'tabContentRight' => [
-                    'image'=> Storage::url('media/others.png'),
-                ],
-            ]],
-        ]
-    ];
-    //End Tab Sektor Industri Temporary Data
 @endphp
 
 <x-layouts.app>
     <x-partials.header />
 
     <main>
-        <x-partials.hero-page :image="$item->featuredImage?->url ?? Storage::url('media/bdc43b57-5e80-418a-9a89-47cf20e59746.jpg')" h1="{{ strip_tags($item->content) ?? 'Mengapa KIW?' }}" />
+        <x-partials.hero-page :image="$item->featuredImage?->url ?? Storage::url('media/bdc43b57-5e80-418a-9a89-47cf20e59746.jpg')" h1="{{ $item->title ?? 'Ekosistem Klaster Bisnis' }}" />
 
 
     <!--Start Tab Sektor Industri-->
@@ -77,18 +32,18 @@
             <!-- Tab Headers -->
             <div
                 class="header-sektor-wrap lg:flex lg:flex-row lg:justify-start lg:gap-5 grid grid-cols-2 gap-2 justify-center z-1">
-                @foreach ($tabSektorIndustri['tabsTitle'] as $index => $item)
-                <x-tab.tab-headers-sektor 
-                    :title="$item['tab']" 
-                    :tab="'tab' . ($index + 1)" 
+                @foreach ($tabSektorIndustri as $index => $item)
+                <x-tab.tab-headers-sektor
+                    :title="$item['tabTitle']"
+                    :tab="'tab' . ($index + 1)"
                 />
-            @endforeach
+                @endforeach
             </div>
 
             <!-- Tab Contents -->
-            @foreach ($tabSektorIndustri['tabsContent'] as $index => $item)
-            <x-tab.tab-contents-sektor id="tab{{ $index + 1 }}" :title="$item['tab']['tabContentLeft']['title']"
-                :image="$item['tab']['tabContentRight']['image']" :desc="$item['tab']['tabContentLeft']['desc']" />
+            @foreach ($tabSektorIndustri as $index => $item)
+            <x-tab.tab-contents-sektor id="tab{{ $index + 1 }}" :title="$item['tabContent']['title']"
+                :image="$item['tabContent']['image']" :desc="$item['tabContent']['description']" />
             @endforeach
         </div>
     </section>
