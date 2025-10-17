d129047e0a93580b5206292167cbc38e0ddbafbd
Daniel <danielbekool@gmail.com>
2025-10-17 17:52:07 +0700

    backup archive bpsp

--- resources/views/templates/archives/archive-buildings.blade.php
+++ resources/views/templates/archives/archive-buildings.blade.php
@@ -1,4 +1,4 @@
-@php
+<!-- @php
     $titleBPSPContent = [
         'title' => 'Bangunan Pabrik Siap Pakai',
         'desc' => 'KIW menghadirkan berbagai pilihan bangunan pabrik siap pakai berkualitas tinggi yang tersedia untuk disewa, dirancang untuk mendukung operasional industri secara langsung.
@@ -24,7 +24,7 @@
             'Ketinggian Halaman: 14 meter di atas permukaan jalan²',
         ]
     ];
-@endphp
+@endphp -->
 <x-layouts.app>
     <x-partials.header />
     <main>
@@ -33,10 +33,10 @@
         <x-partials.hero-page :image="isset($record->featuredImage) ? $record->featuredImage->url : Storage::url('media/bpsp.jpg')" h1="{{ $record->title ?? ($title ?? 'Bangunan Pabrik Siap Pakai') }}" />
         <!--Start Bangunan Pabrik-->
 
-        <section id="bangunan-pabrik"
+        <!-- <section id="bangunan-pabrik"
             class="flex flex-col my-18 lg:my-30 px-4 sm:px-6 lg:px-0 gap-10 lg:gap-20 lg:w-[1200px] lg:mx-auto">
 
-             <!--Title-->
+            
             <div class="flex flex-col mb-18">
                 <div class="flex flex-col lg:flex-row gap-5 ">
                     <div class="flex flex-col gap-5 lg:w-3/4">
@@ -69,6 +69,25 @@
                     </ul>
                 </div>
 
+            </div> -->
+
+            <section id="bangunan-pabrik"
+            class="flex flex-col my-18 lg:my-30 px-4 sm:px-6 lg:px-0 gap-10 lg:gap-20 lg:w-[1200px] lg:mx-auto">
+
+            <!--Title-->
+            <div class="flex flex-col lg:flex-row lg:items-center gap-5 lg:gap-10">
+                <h2 data-aos="fade-up">
+                    BPSP Modern & Fungsional
+                </h2>
+                <div class="flex flex-col gap-5">
+                    <p class="sub-p" data-aos="fade-down">
+                        {!! $record->content !!}
+                    </p>
+                    <p>
+                        KIW juga siap mendirikan BPSP baru untuk memenuhi kebutuhan para investor dalam menjalankan
+                        bisnis. Available BPSP 12 saat ini tersedia disewa dengan spesifikasi berikut.
+                    </p>
+                </div>
             </div>
 
 
