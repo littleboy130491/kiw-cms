6a3f391dda24627920c67bdb96f6078b81959d4b
unknown <littleboy_13@yahoo.com>
2025-10-17 10:32:31 +0700

    Add migration change column exif from curator media

--- database/migrations/2025_10_16_062819_add_giver_to_achievements_table.php.php
+++ database/migrations/2025_10_16_062819_add_giver_to_achievements_table.php.php
@@ -4,25 +4,18 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
+return new class extends Migration {
     public function up(): void
     {
-        Schema::table('achievements', function (Blueprint $table) {
-            $table->json('giver');
+        Schema::table(app(config('curator.model'))->getTable(), function (Blueprint $table) {
+            $table->longText('exif')->nullable()->change();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
-         Schema::table('achievements', function (Blueprint $table) {
-            $table->dropColumn('giver');
+        Schema::table(app(config('curator.model'))->getTable(), function (Blueprint $table) {
+            $table->text('exif')->nullable()->change();
         });
     }
 };
---
+++ database/migrations/2025_10_17_032955_increase_exif_column_length_in_curator_media_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+public function up(): void
+{
+Schema::table(app(config('curator.model'))->getTable(), function (Blueprint $table) {
+$table->longText('exif')->nullable()->change();
+});
+}
+
+public function down(): void
+{
+Schema::table(app(config('curator.model'))->getTable(), function (Blueprint $table) {
+$table->text('exif')->nullable()->change();
+});
+}
+};
