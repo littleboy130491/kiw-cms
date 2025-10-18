41062cd107e00699bf4e81ee00cea22dd76c22a0
Henry <arisman.henry@gmail.com>
2025-10-18 10:43:19 +0700

    Categories lelang from CMS data

--- resources/views/templates/archives/categories-lelang.blade.php
+++ resources/views/templates/archives/categories-lelang.blade.php
@@ -1,3 +1,9 @@
+@php
+    $block = collect($record->block);
+    $section = $block->where('data.block_id', 'section-lelang')->first();
+
+    $description = $section['data']['description'] ?? $record->content ?? '';
+@endphp
 <x-layouts.app>
     <x-partials.header />
     <main>
@@ -11,13 +17,14 @@
             <!--Title-->
             <div class="flex flex-col lg:flex-row lg:items-start gap-5 lg:gap-25">
                 <h2 class="lg:w-1/3" data-aos="fade-up">
-                    Pedoman Lelang
+                    {{$section['data']['title'] ?? 'Pedoman Lelang' }}
                 </h2>
 
                 <div class="lg:w-2/3 flex flex-col gap-5">
-                    {!! $record->content ?? '' !!}
+                    <p>{!! $description !!}</p>
                     <!--button-->
-                    <a class="w-fit btn1 mt-5" data-aos="fade-down" href="#">Unduh Pedoman
+                    <a class="w-fit btn1 mt-5" data-aos="fade-down" href="{{ $section['data']['url'] ?? '#' }}">
+                        {{ $section['data']['button_label'] ?? 'Unduh Pedoman' }}
                         <x-icon.download-icon-current />
                     </a>
                 </div>
