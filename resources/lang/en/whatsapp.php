611920f39a8bbcf47d4d3ee3515c2e3f686442b6
Henry <arisman.henry@gmail.com>
2025-10-18 13:08:55 +0700

    Localization whatsapp message

---
+++ resources/lang/en/whatsapp.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'greeting' => 'Hello! 👋',
+    'welcome_message' => 'Welcome to the official website of <b>PT Kawasan Industri Wijayakusuma (KIW)</b>.',
+    'intro_message' => 'We are ready to help you find the best industrial estate solutions in Indonesia.',
+    'call_to_action' => 'Please chat directly to learn more about industrial land, Ready-to-use Factory Buildings (BPSP), or our integrated service facilities.',
+    'customer_service' => 'KIW Customer Service',
+    'type_message' => 'Type a message',
+];
---
+++ resources/lang/id/whatsapp.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'greeting' => 'Halo! 👋',
+    'welcome_message' => 'Selamat datang di website resmi <b>PT Kawasan Industri Wijayakusuma (KIW)</b>.',
+    'intro_message' => 'Kami siap membantu Anda menemukan solusi kawasan industri terbaik di Indonesia.',
+    'call_to_action' => 'Silakan bisa chat langsung untuk mengetahui lebih lanjut mengenai lahan industri, Bangunan Pabrik Siap Pakai (BPSP), atau fasilitas layanan terpadu kami.',
+    'customer_service' => 'Customer Service KIW',
+    'type_message' => 'Ketik pesan',
+];
