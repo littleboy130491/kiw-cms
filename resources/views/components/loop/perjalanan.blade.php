de6aae0dbd207ede03a1cea2bb716f71eb76a45f
Henry <arisman.henry@gmail.com>
2025-10-19 09:24:19 +0700

    Remove 'tahun'

--- resources/views/components/loop/perjalanan.blade.php
+++ resources/views/components/loop/perjalanan.blade.php
@@ -2,12 +2,12 @@
 <div class="swiper-slide !flex flex-col gap-5 h-auto justify-between overflow-hidden">
 
     <div class="flex flex-col gap-5">
-        <h3 class="text-white w-[80%] text-[3.1em] lg:text-[4.5em]">Tahun {{ $year ?? '0000'  }}</h3>
+        <h3 class="text-white w-[80%] text-[3.1em] lg:text-[4.5em]">{{ $year ?? '0000'  }}</h3>
         <span class="border-t border-[var(--color-bordertransparent)]"></span>
         <p class="text-white">
             {{ $desc }}
         </p>
     </div>
 
-    
+
 </div>
