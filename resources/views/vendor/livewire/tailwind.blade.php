91caf0402fcbcc0738a8271147c6e4e71b48c149
Henry <arisman.henry@gmail.com>
2025-10-24 08:52:28 +0700

    Update pagination to minimalist style

--- resources/views/vendor/livewire/tailwind.blade.php
+++ resources/views/vendor/livewire/tailwind.blade.php
@@ -5,83 +5,81 @@
 
     $scrollIntoViewJsSnippet = ($scrollTo !== false)
         ? <<<JS
-           (\$el.closest('{$scrollTo}') || document.querySelector('{$scrollTo}')).scrollIntoView()
-        JS
+               (\$el.closest('{$scrollTo}') || document.querySelector('{$scrollTo}')).scrollIntoView()
+            JS
         : '';
 @endphp
 
 <div>
     @if ($paginator->hasPages())
-        <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="flex items-center justify-center">
-            <ul class="flex flex-row flex-wrap justify-center">
-                {{-- Previous Page Link --}}
-                @if ($paginator->onFirstPage())
-                    <li class="flex items-center justify-center" aria-disabled="true"
-                        aria-label="{{ __('pagination.previous') }}">
-                        <span class="px-3 py-1 border border-[var(--color-heading)] rounded opacity-50">
-                            {!! __('pagination.previous') !!}
+        <nav role="navigation" aria-label="Pagination Navigation"
+            class="flex items-center justify-center w-full px-2 sm:px-4">
+            <div class="w-full max-w-full overflow-x-auto scrollbar-hide">
+                <div class="mx-auto inline-flex items-center whitespace-nowrap text-base font-medium py-2 gap-2">
+                    {{-- Previous Page Link --}}
+                    @if ($paginator->onFirstPage())
+                        <span
+                            class="inline-flex items-center justify-center px-2 sm:px-2.5 py-1 text-sm sm:text-base rounded-md opacity-40 cursor-default">
+                            <span class="hidden sm:inline">{!! __('pagination.previous') !!}</span>
+                            <span class="sm:hidden">Prev</span>
                         </span>
-                    </li>
-                @else
-                    <li class="flex items-center justify-center">
+                    @else
                         <button type="button" wire:click="previousPage('{{ $paginator->getPageName() }}')"
                             x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled"
-                            dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before"
-                            class="px-3 py-1 border border-[var(--color-heading)] hover:bg-[var(--color-blue)] hover:text-white hover:border-[var(--color-blue)] rounded cursor-pointer">
-                            {!! __('pagination.previous') !!}
+                            dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}"
+                            class="inline-flex items-center justify-center px-2 sm:px-2.5 py-1 text-sm sm:text-base rounded-md cursor-pointer transition-colors hover:text-[var(--color-blue)] active:scale-95">
+                            <span class="hidden sm:inline">{!! __('pagination.previous') !!}</span>
+                            <span class="sm:hidden">Prev</span>
                         </button>
-                    </li>
-                @endif
-
-                {{-- Pagination Elements --}}
-                @foreach ($elements as $element)
-                    {{-- "Three Dots" Separator --}}
-                    @if (is_string($element))
-                        <li class="flex items-center justify-center" aria-disabled="true">
-                            <span class="px-3 py-1 border border-[var(--color-heading)] rounded">{{ $element }}</span>
-                        </li>
                     @endif
 
-                    {{-- Array Of Links --}}
-                    @if (is_array($element))
-                        @foreach ($element as $page => $url)
-                            @if ($page == $paginator->currentPage())
-                                <li class="flex items-center justify-center" aria-current="page">
-                                    <div class="current-page px-3 py-1 bg-[var(--color-blue)] rounded text-white">
-                                        {{ $page }}
-                                    </div>
-                                </li>
-                            @else
-                                <li class="flex items-center justify-center">
-                                    <button type="button" wire:click="gotoPage({{ $page }}, '{{ $paginator->getPageName() }}')"
-                                        x-on:click="{{ $scrollIntoViewJsSnippet }}"
-                                        class="px-3 py-1 border border-[var(--color-heading)] hover:bg-[var(--color-blue)] hover:text-white hover:border-[var(--color-blue)] rounded cursor-pointer">
-                                        {{ $page }}
-                                    </button>
-                                </li>
+                    <div class="inline-flex items-center gap-1">
+                        {{-- Pagination Elements --}}
+                        @foreach ($elements as $element)
+                            {{-- "Three Dots" Separator --}}
+                            @if (is_string($element))
+                                <span
+                                    class="inline-flex items-center justify-center w-7 sm:w-8 py-1 text-sm sm:text-base rounded-md">{{ $element }}</span>
+                            @endif
+
+                            {{-- Array Of Links --}}
+                            @if (is_array($element))
+                                @foreach ($element as $page => $url)
+                                    @if ($page == $paginator->currentPage())
+                                        <span
+                                            class="inline-flex items-center justify-center w-7 sm:w-8 py-1 text-sm sm:text-base rounded-md text-[var(--color-blue)] font-semibold">
+                                            {{ $page }}
+                                        </span>
+                                    @else
+                                        <button type="button" wire:click="gotoPage({{ $page }}, '{{ $paginator->getPageName() }}')"
+                                            x-on:click="{{ $scrollIntoViewJsSnippet }}"
+                                            class="inline-flex items-center justify-center w-7 sm:w-8 py-1 text-sm sm:text-base rounded-md cursor-pointer transition-colors hover:text-[var(--color-blue)] active:scale-95">
+                                            {{ $page }}
+                                        </button>
+                                    @endif
+                                @endforeach
                             @endif
                         @endforeach
-                    @endif
-                @endforeach
+                    </div>
 
-                {{-- Next Page Link --}}
-                @if ($paginator->hasMorePages())
-                    <li class="flex items-center justify-center">
+                    {{-- Next Page Link --}}
+                    @if ($paginator->hasMorePages())
                         <button type="button" wire:click="nextPage('{{ $paginator->getPageName() }}')"
                             x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled"
-                            dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}.before"
-                            class="px-3 py-1 border border-[var(--color-heading)] hover:bg-[var(--color-blue)] hover:text-white hover:border-[var(--color-blue)] rounded cursor-pointer">
-                            {!! __('pagination.next') !!}
+                            dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}"
+                            class="inline-flex items-center justify-center px-2 sm:px-2.5 py-1 text-sm sm:text-base rounded-md cursor-pointer transition-colors hover:text-[var(--color-blue)] active:scale-95">
+                            <span class="hidden sm:inline">{!! __('pagination.next') !!}</span>
+                            <span class="sm:hidden">Next</span>
                         </button>
-                    </li>
-                @else
-                    <li class="flex items-center justify-center" aria-disabled="true" aria-label="{{ __('pagination.next') }}">
-                        <span class="px-3 py-1 border border-[var(--color-heading)] rounded opacity-50">
-                            {!! __('pagination.next') !!}
+                    @else
+                        <span
+                            class="inline-flex items-center justify-center px-2 sm:px-2.5 py-1 text-sm sm:text-base rounded-md opacity-40 cursor-default">
+                            <span class="hidden sm:inline">{!! __('pagination.next') !!}</span>
+                            <span class="sm:hidden">Next</span>
                         </span>
-                    </li>
-                @endif
-            </ul>
+                    @endif
+                </div>
+            </div>
         </nav>
     @endif
 </div>
