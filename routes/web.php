350e33fdce3f3cb8a41bd9334fdc7a4838af17ed
Henry <arisman.henry@gmail.com>
2025-10-24 10:09:37 +0700

    Redirect rules from old site

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,25 @@
 <?php
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 
+// Custom redirect rules (from old site)
+Route::get('tentang-perusahaan', function () {
+    $defaultLang = Config::get('cms.default_language', 'en');
+    return redirect()->route('cms.page', ['lang' => $defaultLang, 'slug' => 'profil-perusahaan']);
+});
+
+Route::get('whistle-blowing-system-wbs-pengendalian-gratifikasi', function () {
+    $defaultLang = Config::get('cms.default_language', 'en');
+    return redirect()->route('cms.page', ['lang' => $defaultLang, 'slug' => 'whistleblowing']);
+});
+
+Route::get('tag/direksi-pt-kiw-persero/{any?}', function ($any = null) {
+    $defaultLang = Config::get('cms.default_language', 'en');
+    return redirect()->route('cms.page', ['lang' => $defaultLang, 'slug' => 'manajemen-perusahaan']);
+})->where('any', '.*');
+
+Route::get('category/{any}', function ($any) {
+    $defaultLang = Config::get('cms.default_language', 'en');
+    return redirect()->to('/' . $defaultLang . '/categories/' . $any);
+});
